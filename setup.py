5033549ab521ca0a4e6646c8660ff00dcba0f1a7
Jim Graszer <jim@lemmalegal.com>
2025-09-26 12:54:16 -0500

    Updated the program from the review feedback.

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.UIHearingRequest',
-      version='0.1.0',
+      version='0.1.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.UIHearingRequest\n\nA docassemble extension.\n\n## Author\n\nMatt Newsted, mnewsted@illinoislegalaid.org\n\n',
+      long_description='# docassemble.UIHearingRequest\n\nA docassemble extension.\n\n## Author\n\nJim Graszer, Lemma Legal (jim@lemmalegal.com)\n\nMatt Newsted, mnewsted@illinoislegalaid.org\n\n',
       long_description_content_type='text/markdown',
       author='Jim Graszer',
       author_email='jim@lemmalegal.com',
