5ef636dcf556a58e883b945f9c29737e8caf2585
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-09-29 10:59:57 -0500

    ready for internal testing

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.UIHearingRequest',
-      version='0.1.3',
+      version='0.1.4',
       description=('A docassemble extension.'),
-      long_description='# docassemble.UIHearingRequest\n\nA docassemble extension.\n\n## Author\n\nJim Graszer, Lemma Legal (jim@lemmalegal.com)\n\nMatt Newsted, mnewsted@illinoislegalaid.org\n\n',
+      long_description='# docassemble.UIHearingRequest\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Jim Graszer',
       author_email='jim@lemmalegal.com',
