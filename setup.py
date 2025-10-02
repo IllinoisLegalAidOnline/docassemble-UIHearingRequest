8702920ff0c19b4649336b3ac649f1afccbf4959
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-10-02 10:16:07 -0500

    additional interview and form tweaks

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.UIHearingRequest',
-      version='0.1.4',
+      version='0.1.5',
       description=('A docassemble extension.'),
       long_description='# docassemble.UIHearingRequest\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
